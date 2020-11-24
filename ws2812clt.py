a38d4ac33ae727ba30706912a9ecc53713af0020
Christoph Pranzl <christoph.pranzl@pranzl.net>
2020-11-24 14:22:00 +0100

    Adjusted to API changes of server

--- ws2812clt.py
+++ ws2812clt.py
@@ -2,11 +2,29 @@
 # -*- coding: utf-8 -*
 
 import xmlrpc.client
+from rpi_ws281x import Color
+
+BLACK = Color(0, 0, 0)
+RED = Color(255, 0, 0)
+GREEN = Color(0, 255, 0)
+BLUE = Color(0, 0, 255)
+TEAL = Color(0, 128, 128)
+CYAN = Color(0, 255, 255)
+WHITE = Color(255, 255, 255)
 
 ws2812srv = xmlrpc.client.ServerProxy('http://localhost:8000')
 
-print(ws2812srv.addAnimation(5, 'powerup'))
-print(ws2812srv.addAnimation(5, 'powerup'))
-print(ws2812srv.addAnimation(1, 'wipe'))
-print(ws2812srv.clrAnimations())
-print(ws2812srv.addAnimation(5, 'powerdown'))
+print(ws2812srv.chgBrightness(25))
+print(ws2812srv.addAnimation('powerup', CYAN, 0, 0))
+print(ws2812srv.addAnimation('powerdown', BLACK, 0, 0))
+print(ws2812srv.chgBrightness(100))
+print(ws2812srv.addAnimation('powerup', CYAN, 0, 0))
+print(ws2812srv.addAnimation('powerdown', BLACK, 0, 0))
+
+# print(ws2812srv.addAnimation('powerup', CYAN))
+# print(ws2812srv.addAnimation('powerup', CYAN))
+# print(ws2812srv.addAnimation('wipe', BLACK))
+# print(ws2812srv.clrAnimations())
+# print(ws2812srv.addAnimation('powerdown', BLACK))
+# print(ws2812srv.addAnimation('chgvolume', GREEN, 25, 25))
+# print(ws2812srv.addAnimation('chgvolume', GREEN, 50, -60))
